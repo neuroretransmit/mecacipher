05397c4add525bfdaa638c893dfd6d1e3e10aca8
John Holly <j@optimistic.ninja>
2022-02-12 07:57:55 -0700

    Added test for 16-bit word sizes

--- tests/mecacipher.cc
+++ tests/mecacipher.cc
@@ -15,6 +15,38 @@ template<typename T> ostream& operator<<(ostream& output, vector<T> const& value
     return output;
 }
 
+TEST(MECACipher, Crypt64)
+{
+    MECACipher<uint16_t, 15> cipher;
+    string key = "My crime is that of curiosity";
+
+    vector<uint16_t> plaintext{0x1337, 0x1337, 0x1337, 0x1337};
+    vector<uint16_t> key_words = key_to_words<uint16_t>(key);
+    vector<uint16_t> key_schedule = cipher.schedule_keys(key_words);
+
+    // Encrypt
+    auto enc_start = high_resolution_clock::now();
+    vector<uint16_t> encrypted = cipher.encrypt(plaintext, key_schedule);
+    auto enc_stop = high_resolution_clock::now();
+    auto enc_duration = duration_cast<microseconds>(enc_stop - enc_start);
+
+    // Decrypt
+    auto dec_start = high_resolution_clock::now();
+    vector<uint16_t> decrypted = cipher.decrypt(encrypted, key_schedule);
+    auto dec_stop = high_resolution_clock::now();
+    auto dec_duration = duration_cast<microseconds>(dec_stop - dec_start);
+
+    cout << "plaintext: " << plaintext << endl;
+    cout << "key      : " << key_words << endl;
+    cout << "encrypted: " << encrypted << endl;
+    cout << "decrypted: " << decrypted << endl;
+
+    cout.unsetf(ios::hex);
+    cout << "enc time : " << enc_duration.count() << " microseconds" << endl;
+    cout << "dec time : " << dec_duration.count() << " microseconds" << endl;
+    ASSERT_EQ(plaintext, decrypted);
+}
+
 TEST(MECACipher, Crypt128)
 {
     MECACipher<uint32_t, 15> cipher;
